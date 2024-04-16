6b74d63559e8f0d62e49d22388d2c46109be357b
killbill1990 <72857247+killbill1990@users.noreply.github.com>
2024-04-16 21:51:18 +0300

    Add files via upload

---
+++ Exercise/ComboBox/ComboBox/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data;
+using System.Data.SqlClient;
+
+
+namespace ComboBox
+{
+    public partial class Form1 : Form
+    {
+        public static SqlConnection con = new SqlConnection(@"Data Source=DESKTOP-OKIJB0T;Initial Catalog=data1;Integrated Security=True");
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            con.Open();
+
+            SqlCommand cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "select * from fileRelation";
+            cmd.ExecuteNonQuery();
+            DataTable dt = new DataTable();
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            da.Fill(dt);
+            foreach (DataRow dr in dt.Rows)
+            {
+                
+                comboBox1.Items.Add(dr["keyCase"].ToString());
+               
+            }
+
+            con.Close();
+        }
+
+       
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+            con.Open();
+            SqlCommand cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            //cmd.CommandText = "select * from fileTelephone  where keyPerson='"+comboBox1.SelectedItem.ToString()+"'";
+            cmd.CommandText = "select filePayment.amountPay,filePayment.clews,fileTelephone.telephone from filePayment  join fileRelation on filePayment.keyCase = fileRelation.keyCase join fileTelephone on fileRelation.keyPerson = fileTelephone.keyPerson   where fileRelation.keyCase = '" + comboBox1.SelectedItem.ToString() + "'";
+            cmd.ExecuteNonQuery();
+            DataTable dt = new DataTable();
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            da.Fill(dt);
+            foreach (DataRow dr in dt.Rows)
+            {
+                textBox1.Text = dr["telephone"].ToString();
+                textBox2.Text = dr["amountPay"].ToString();
+                textBox3.Text = dr["clews"].ToString();
+
+            }
+
+           
+            con.Close();
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            
+           
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            
+
+        }
+
+       
+    }
+}
---
+++ Exercise/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ionic.Zip;
+using System.Data.SqlClient;
+
+
+using Microsoft.SqlServer.Dts.Runtime;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+       
+
+        static void Main(string[] args)
+        {
+
+
+
+
+
+
+            //Here is the path of the folder where there are the files
+            string path = @"D:\SoftwareDeveloperExercise\";
+
+             //Unzip the zip file with the text files
+             using (ZipFile archive = new ZipFile(@"D:\SoftwareDeveloperExercise\Files.zip"))
+             {
+                 archive.Password = "FirstCall13";
+                 archive.Encryption = EncryptionAlgorithm.WinZipAes256;
+                 archive.StatusMessageTextWriter = Console.Out;
+
+                 archive.ExtractAll(@"D:\SoftwareDeveloper", ExtractExistingFileAction.Throw);
+             }
+
+
+            var application = new Application();
+            using (var package = application.LoadPackage(@"C:\Users\Vasilis\source\repos\SsisLoadData\SsisLoadData\Package.dtsx", null))
+            {
+                var execResult = package.Execute();
+                Console.WriteLine(execResult.ToString());
+            }
+            Console.ReadKey();
+
+
+
+        }
+    }
+
+
+}
